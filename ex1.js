38bc64c57696e413c3960a495b43f2b52c4fee63
Agatha Karenne <142344001+AgathaKarenne32@users.noreply.github.com>
2025-04-30 21:28:53 -0300

    Delete ex1.js

--- ex1.js
+++
@@ -1,9 +0,0 @@
-const prompt = require('prompt-sync')()
-
-let numero = Number(prompt('Escreva um número inteiro: '))
-
-if (numero % 2 === 0) {
-    console.log('Esse número é par')
-} else {
-    console.log('Esse número é ímpar')
-}
