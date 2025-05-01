71ea477707d5135cf826051e6840f12e3ac4ac5b
Agatha Karenne <142344001+AgathaKarenne32@users.noreply.github.com>
2025-04-30 21:29:37 -0300

    Delete ex12.js

--- ex12.js
+++
@@ -1,9 +0,0 @@
-const prompt = require('prompt-sync')()
-
-let numero = Number(prompt('digite o numero que deseja saber a tabuada: '))
-let tabuada
-
-for (let i = 1; i <= 10; i++) {
-    let tabuada = i * numero
-    console.log(`${numero} x ${i} = ${tabuada}`)
-}
